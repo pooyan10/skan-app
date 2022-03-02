40feec37e5cf77c95a1fd931382eb45010b36228
Pouyan Hamidi <pouyan@Pouyans-MacBook-Pro.local>
2022-03-03 00:08:20 +0330

    adding bar of progress

--- Components/InfoCard.js
+++ Components/InfoCard.js
@@ -1,13 +1,41 @@
 import Image from "next/image";
+import { HeartIcon } from "@heroicons/react/outline";
+import { StarIcon } from "@heroicons/react/solid";
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div>
-      <div className="relative h-24 w-40">
-        <Image src={img} layout="fill" objectFit="cover" />
+    <div className="flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-xl transition duration-200 ease-out first:border-t ">
+      <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
+        <Image
+          className="rounded-xl"
+          src={img}
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
 
-      <div></div>
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="h-7 cursor-pointer active:text-red-500 active:scale-110 transition duration-200 ease-out" />
+        </div>
+        <h4 className="text-xl">{title}</h4>
+
+        <div className="border-b w-10 pt-2" />
+
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description} </p>
+
+        <div className="flex justify-between items-end pt-5">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {star}
+          </p>
+          <div className="">
+            <p className="text-lg lg:text-2xl font-semibold pb-2">{price}</p>
+            <p className="font-extralight text-right text-gray-500">{total}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
